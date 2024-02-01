3bce02ecf4413ba04dff1091144eafed4e700c1f
soda <86339660+meirisoda@users.noreply.github.com>
2024-01-31 23:45:02 -0500

    add ,... for new files

--- configuration/hosts/XPS.nix
+++ configuration/hosts/XPS.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, stablepkgs }:
+{ config, pkgs, stablepkgs, ... }:
 
 {
  imports = [
--- configuration/hosts/XPS/nginx.nix
+++ configuration/hosts/XPS/nginx.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, stablepkgs, attrs}: 
+{ config, pkgs, stablepkgs, attrs, ...}: 
 
 {
   services.nginx = {
