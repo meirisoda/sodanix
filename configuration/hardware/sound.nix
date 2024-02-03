1a8dd84e549d0a3b7d5de4cf6b810c90fc79f939
soda <86339660+meirisoda@users.noreply.github.com>
2024-02-03 12:20:44 -0500

    fix pipewire

--- configuration/hardware/sound.nix
+++ configuration/hardware/sound.nix
@@ -9,7 +9,21 @@
     audio.enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
-    jack.enable = true;
+    # jack.enable = true;
     pulse.enable = true;
+    config.pipewire = {
+      "context.properties" = [
+        {
+          "link.max-buffers" = 64;
+          "log.level" = 2;
+          "default.clock.rate" = 48000;
+          "default.clock.quantum" = 1024;
+          "default.clock.min-quantum" = 32;
+          "default.clock.max-quantum" = 8192;
+        }
+      ];
+    };
+    # Fix for pipewire-pulse breaking recently
+  systemd.user.services.pipewire-pulse.path = [ pkgs.pulseaudio ];
   };
 }
