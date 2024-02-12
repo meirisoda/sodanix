094651891c2f6b9f030fb06bcb4ea673aef6f630
meirisoda <meirisoda@proton.me>
2024-02-11 19:19:08 -0500

    disable offload mode and attempt sync mode -- changed semicolons

--- configuration/hosts/ROG.nix
+++ configuration/hosts/ROG.nix
@@ -28,7 +28,7 @@
       # offload = {
         # enable =  true;
         # enableOffloadCmd = true; # Provides `nvidia-offload` command.
-      };
+      # };
       nvidiaBusId = "PCI:1:0:0";
       amdgpuBusId = "PCI:65:0:0";
     };
