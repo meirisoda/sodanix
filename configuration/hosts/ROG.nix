279144667f982db6a0ea2c465adaefedbdb87823
meirisoda <meirisoda@proton.me>
2024-02-11 19:22:26 -0500

    go back to reverse sync

--- configuration/hosts/ROG.nix
+++ configuration/hosts/ROG.nix
@@ -21,14 +21,14 @@
     nvidiaSettings = true;
     open = false;
     prime = {
-      # reverseSync.enable = true; 
-      sync = {
-       enable = true;
-      };
-      # offload = {
-        # enable =  true;
-        # enableOffloadCmd = true; # Provides `nvidia-offload` command.
+      reverseSync.enable = true; 
+      # sync = {
+      #  enable = true;
       # };
+      offload = {
+        enable =  true;
+        enableOffloadCmd = true; # Provides `nvidia-offload` command.
+      };
       nvidiaBusId = "PCI:1:0:0";
       amdgpuBusId = "PCI:65:0:0";
     };
