0318b13d311c03485223f48d18ef756d9213e7a8
meirisoda <meirisoda@proton.me>
2024-02-12 22:17:19 -0500

    remove theme

--- configuration/soda/vscode.nix
+++ configuration/soda/vscode.nix
@@ -9,7 +9,7 @@
       eamodio.gitlens
       ritwickdey.liveserver
       jnoortheen.nix-ide
-      LaurentTreguier.simple-icons
+      # LaurentTreguier.simple-icons
       vscode_exts.yzane.markdown-pdf
       vscode_exts.aaron-bond.better-comments
       vscode_exts.ziyasal.vscode-open-in-github
