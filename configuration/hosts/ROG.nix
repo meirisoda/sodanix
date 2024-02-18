9e1de9f9ff0eabb0a794ba1a51a9142d6ba0ff24
meirisoda <meirisoda@proton.me>
2024-02-17 22:36:30 -0500

    change to beta nvidia driver - 550.40

--- configuration/hosts/ROG.nix
+++ configuration/hosts/ROG.nix
@@ -30,6 +30,6 @@
       nvidiaBusId = "PCI:1:0:0";
       amdgpuBusId = "PCI:65:0:0";
     };
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
 }
