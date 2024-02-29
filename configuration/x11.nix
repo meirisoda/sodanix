065b9abd0988aa83addbcac9937340b5e439cdea
meirisoda <meirisoda@proton.me>
2024-02-29 16:13:43 -0500

    remove kwin_drm_device

--- configuration/x11.nix
+++ configuration/x11.nix
@@ -39,9 +39,10 @@
     KWIN_DRM_USE_MODIFIERS = "0";
     ENABLE_VKBASALT = "1";
     GLFW_IM_MODULE = "ibus"; # kitty only supports ibus afaik
-  } // (if hostname == "sodaROG" then {
-      KWIN_DRM_DEVICES = "/dev/dri/by-path/pci-0000\\:65\\:00.0-card";
-    } else {});
+  } 
+  # // (if hostname == "sodaROG" then {
+  #     KWIN_DRM_DEVICES = "/dev/dri/by-path/pci-0000\\:65\\:00.0-card";
+  #   } else {});
 
 # TODO hardware opengl
   hardware.opengl = with pkgs; {
