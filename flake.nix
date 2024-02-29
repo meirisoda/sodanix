38510a413277dc6d8dd2fda87b07d634ebcec4e5
meirisoda <meirisoda@proton.me>
2024-02-29 14:56:51 -0500

    add plasma-manager back

--- flake.nix
+++ flake.nix
@@ -16,9 +16,9 @@
       url = "github:Mic92/nix-index-database";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # plasma-manager = {
-    #   url = "github:pjones/plasma-manager";
-    # };
+    plasma-manager = {
+      url = "github:pjones/plasma-manager";
+    };
     sodasddm = {
       url = "github:meirisoda/kde-plasma-sodasddm";
       flake = false;
@@ -28,7 +28,7 @@
     };
   }; 
 
-  outputs = { nixpkgs, nixpkgs-stable, home-manager, nixos-hardware, nix-index-database, sodasddm, watershot,... }@attrs: let 
+  outputs = { nixpkgs, nixpkgs-stable, home-manager, nixos-hardware, nix-index-database, sodasddm, plasma-manager, watershot,... }@attrs: let 
     config = { 
       allowUnfree = true; 
       cudaSupport = true;
