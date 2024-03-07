84a95a26961d30bb5fa09a423af59240350cdc18
meirisoda <meirisoda@proton.me>
2024-03-07 15:07:14 -0500

    revert back to nvidia

--- configuration/hosts/ROG.nix
+++ configuration/hosts/ROG.nix
@@ -5,7 +5,7 @@
     xserver = {
       videoDrivers = [
         "amdgpu"
-        "nouveau"
+        "nvidia"
       ];
     };
     asusd = {
@@ -14,16 +14,6 @@
     };
     # power 
     power-profiles-daemon.enable = true;
-    # tlp = {
-      # enable = true;
-      # settings = {
-        # USB_AUTOSUSPEND=0;
-        # CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-        # CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-# 
-        # STOP_CHARGE_THRESH_BAT0 = 95;
-      # };
-    # };
   };
 
   # hardware.amdgpu = {
@@ -31,34 +21,34 @@
   #   amdvlk = true;
   # };
 
-  # hardware.nvidia = {
-  #   modesetting.enable = true;
-  #   nvidiaSettings = true;
-  #   open = false;
-  #   powerManagement = {
-  #     enable = true;
-  #     finegrained = true; 
-  #   };
-  #   prime = {
-  #     reverseSync.enable = true; 
-  #     # sync = {
-  #     #  enable = true;
-  #     # };
-  #     offload = {
-  #       # enable =  true;
-  #       enableOffloadCmd = true;
-  #     };
-  #     nvidiaBusId = "PCI:1:0:0";
-  #     amdgpuBusId = "PCI:65:0:0";
-  #   };
-  #   package = config.boot.kernelPackages.nvidiaPackages.beta;
-  # };
+  hardware.nvidia = {
+    modesetting.enable = true;
+    nvidiaSettings = true;
+    open = false;
+    powerManagement = {
+      enable = true;
+      finegrained = true; 
+    };
+    prime = {
+      reverseSync.enable = true; 
+      # sync = {
+      #  enable = true;
+      # };
+      offload = {
+        # enable =  true;
+        enableOffloadCmd = true;
+      };
+      nvidiaBusId = "PCI:1:0:0";
+      amdgpuBusId = "PCI:65:0:0";
+    };
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
+  };
 
   boot.kernelParams = [
     "amdgpu.gpu_recovery=1"
     "amdgpu.sg_display=0"
     "amdgpu.dcdebugmask=0x10"
-    "nouveau.config=NvGspRm=1"
+    # "nouveau.config=NvGspRm=1"
   ];
 
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
