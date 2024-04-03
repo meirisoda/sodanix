e05e4992c1d835f9a2beadf9a4847a8c42dd400d
meirisoda <meirisoda@proton.me>
2024-04-03 15:10:20 -0400

    more power stuff

--- configuration/hosts/ROG.nix
+++ configuration/hosts/ROG.nix
@@ -70,23 +70,23 @@
   #   enable = true;
   # };
 
-  # programs.auto-cpufreq = {
-  #   enable = true;
-  #   settings = {
-  #     charger = {
-  #       governor = "performance";
-  #       scaling_max_freq = 3300000; 
-  #       turbo = "never";
-  #     };
-  #     battery = {
-  #       governor = "powersave";
-  #       scaling_min_freq = 400000;
-  #       scaling_max_freq = 2000000;
-  #       turbo = "never";
-  #       enable_thresholds = true;
-  #       stop_threshold = 95;
-  #     };
-  #   };
-  # };
+  programs.auto-cpufreq = {
+    enable = true;
+    settings = {
+      charger = {
+        governor = "performance";
+        scaling_max_freq = 3300000; 
+        turbo = "never";
+      };
+      battery = {
+        governor = "powersave";
+        scaling_min_freq = 400000;
+        scaling_max_freq = 2000000;
+        turbo = "never";
+        enable_thresholds = true;
+        stop_threshold = 95;
+      };
+    };
+  };
 
 }
