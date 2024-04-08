45bdea17c8bdc8598ccd421c612792c1ed044bca
meirisoda <meirisoda@proton.me>
2024-04-07 23:09:09 -0400

    import hyprland

--- configuration/soda/packages_config.nix
+++ configuration/soda/packages_config.nix
@@ -4,7 +4,7 @@ allConfig@{ config, pkgs, username, stablepkgs, attrs, ...}:
   imports =
     [ 
       (import ./config/comma.nix (allConfig // {inherit username;}))
-      # ./config/hyprland.nix
+      ./config/hyprland.nix
       # ./config/plasma.nix
       # (pkgs.callPackage ./config/rofi.nix allConfig)
       ./config/vscode.nix
