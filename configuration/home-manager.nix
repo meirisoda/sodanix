782e5249d0d8b101de5faa269e6479ce23ca2f40
meirisoda <meirisoda@proton.me>
2024-04-15 00:21:04 -0400

    dunst...

--- configuration/home-manager.nix
+++ configuration/home-manager.nix
@@ -19,7 +19,7 @@
           global = {
             width = 300;
             height = 300;
-            offset = "30x50";
+            offset = "30x30";
             origin = "top-right";
             transparency = 10;
             frame_color = "#eceff1";
