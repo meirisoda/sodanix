9220afff2e63d76c23f876e692c4b87f9ca1fb83
meirisoda <meirisoda@proton.me>
2024-04-14 23:59:43 -0400

    enable hyprland...

--- configuration/soda/default.nix
+++ configuration/soda/default.nix
@@ -4,7 +4,7 @@ allConfig@{ config, pkgs, username, stablepkgs, attrs, ...}:
   imports =
     [ 
       (import ./config/programs/comma.nix (allConfig // {inherit username;}))
-      # (import ./config/hyprland.nix (allConfig // {inherit username;}))
+      (import ./config/hyprland.nix (allConfig // {inherit username;}))
       # (import ./config/programs/plasma.nix (allConfig // {inherit username;}))
       # (pkgs.callPackage ./config/rofi.nix allConfig)
       ./config/programs/vscode.nix
