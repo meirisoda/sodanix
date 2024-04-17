f6a8cfc16b43abce100aedc8942e6df8b5aab90f
meirisoda <meirisoda@proton.me>
2024-04-17 10:33:37 -0400

    add hyprland flake

--- configuration/soda/config/hyprland.nix
+++ configuration/soda/config/hyprland.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, stablepkgs, username, hostname, allConfig, ... }@args :
+{ config, pkgs, stablepkgs, username, hostname, ... }@args :
 {
   home-manager.users.${username} = {
     xdg.configFile = {
