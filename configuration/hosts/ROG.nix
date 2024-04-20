bafa1fcfde51ceb27bc889f2507fe9f3a9c9133e
meirisoda <meirisoda@proton.me>
2024-04-19 22:46:11 -0400

    tlp

--- configuration/hosts/ROG.nix
+++ configuration/hosts/ROG.nix
@@ -22,8 +22,8 @@
     tlp = {
       enable = true;
       settings = {
-        START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
-        STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+        START_CHARGE_THRESH_BAT0 = 40;
+        STOP_CHARGE_THRESH_BAT0 = 95; 
 
         CPU_SCALING_GOVERNOR_ON_AC = "performance";
         CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
