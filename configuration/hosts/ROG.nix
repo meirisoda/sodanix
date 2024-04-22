291c38f300b934a271af4c7fcf3ba75a7925316d
meirisoda <meirisoda@proton.me>
2024-04-22 17:11:38 -0400

    balanced on ac

--- configuration/hosts/ROG.nix
+++ configuration/hosts/ROG.nix
@@ -25,8 +25,8 @@
         START_CHARGE_THRESH_BAT0 = 40;
         STOP_CHARGE_THRESH_BAT0 = 95; 
 
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
-        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_AC = "balanced";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "balanced";
 
         CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
         CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
