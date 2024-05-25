1456fd0c4f1d2d84025e621bc3481307132e9549
meirisoda <meirisoda@proton.me>
2024-05-25 15:04:55 -0400

    change hypridle

--- configuration/soda/config/hyprland/hypridle.nix
+++ configuration/soda/config/hyprland/hypridle.nix
@@ -12,26 +12,26 @@ listener {
     on-resume = ${pkgs.brightnessctl}/bin/brightnessctl -r                 # monitor backlight restore.
 }
 
-# turn off keyboard backlight, comment out this section if you dont have a keyboard backlight.
-listener { 
-    timeout = 150                                          # 2.5min.
-    on-timeout = ${pkgs.brightnessctl}/bin/brightnessctl -sd rgb:kbd_backlight set 0 # turn off keyboard backlight.
-    on-resume = ${pkgs.brightnessctl}/bin/brightnessctl -rd rgb:kbd_backlight        # turn on keyboard backlight.
-}
+# # turn off keyboard backlight, comment out this section if you dont have a keyboard backlight.
+# listener { 
+#     timeout = 150                                          # 2.5min.
+#     on-timeout = ${pkgs.brightnessctl}/bin/brightnessctl -sd rgb:kbd_backlight set 0 # turn off keyboard backlight.
+#     on-resume = ${pkgs.brightnessctl}/bin/brightnessctl -rd rgb:kbd_backlight        # turn on keyboard backlight.
+# }
 
-listener {
-    timeout = 720                                 # 12min
-    # on-timeout = loginctl lock-session            # lock screen when timeout has passed
-}
+# listener {
+#     timeout = 720                                 # 12min
+#     # on-timeout = loginctl lock-session            # lock screen when timeout has passed
+# }
 
-listener {
-    timeout = 900                                 # 15min
-    on-timeout = hyprctl dispatch dpms off        # screen off when timeout has passed
-    on-resume = hyprctl dispatch dpms on          # screen on when activity is detected after timeout has fired.
-}
+# listener {
+#     timeout = 900                                 # 15min
+#     on-timeout = hyprctl dispatch dpms off        # screen off when timeout has passed
+#     on-resume = hyprctl dispatch dpms on          # screen on when activity is detected after timeout has fired.
+# }
 
-listener {
-    timeout = 1800                                # 30min
-    on-timeout = systemctl suspend                # suspend pc
-}
-''
+# listener {
+#     timeout = 1800                                # 30min
+#     on-timeout = systemctl suspend                # suspend pc
+# }
+# ''
