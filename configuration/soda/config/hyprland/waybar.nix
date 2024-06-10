d821c53d5d8ae11971ef2e831814176932c36364
meirisoda <meirisoda@proton.me>
2024-06-09 23:21:20 -0400

    waybar spacing?

--- configuration/soda/config/hyprland/waybar.nix
+++ configuration/soda/config/hyprland/waybar.nix
@@ -171,11 +171,11 @@ builtins.toJSON {
     };
     "pulseaudio" = {
       # "scroll-step" = 1; # %; can be a float
-      "format" = "{volume}% {icon} {format_source}";
+      "format" = "{volume}% {icon}   {format_source}";
       "format-bluetooth" = "{volume}% {icon} {format_source}";
       "format-bluetooth-muted" = " {icon} {format_source}";
       "format-muted" = " {format_source}";
-      "format-source" = "{volume}%   ";
+      "format-source" = "{volume}% ";
       "format-source-muted" = "";
       "format-icons" = {
         "headphone" = "";
