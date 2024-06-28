fdcf8a4ed4a9027d7fcb0af543e4aed431169c74
meirisoda <meirisoda@proton.me>
2024-06-28 17:44:38 -0400

    enable nvidia

--- configuration/hosts/ROG.nix
+++ configuration/hosts/ROG.nix
@@ -104,7 +104,8 @@
       nvidiaBusId = "PCI:1:0:0";
       amdgpuBusId = "PCI:65:0:0";
     };
-    # package = config.boot.kernelPackages.nvidiaPackages.latest; 👈 ENABLE NVIDIA
+    package = config.boot.kernelPackages.nvidiaPackages.latest; 
+    # 👈 ENABLE NVIDIA
   };
 
   boot.kernelParams = [
