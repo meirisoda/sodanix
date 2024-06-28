d6a4873a838e29040afcd255147ba9154fe95cae
meirisoda <meirisoda@proton.me>
2024-06-28 18:38:17 -0400

    add onthego specialization - crossing fingers

--- configuration/hosts/ROG.nix
+++ configuration/hosts/ROG.nix
@@ -119,4 +119,15 @@
 
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
+  specialisation = {
+    on-the-go.configuration = {
+      system.nixos.tags = [ "on-the-go" ];
+      hardware.nvidia = {
+        prime.offload.enable = lib.mkForce true;
+        prime.offload.enableOffloadCmd = lib.mkForce true;
+        prime.sync.enable = lib.mkForce false;
+      };
+    };
+  };
+
 }
