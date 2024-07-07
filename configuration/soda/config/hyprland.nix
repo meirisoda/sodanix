b3c018b6e61ca29b9c53a1e0a38635111522b336
meirisoda <meirisoda@proton.me>
2024-07-06 23:49:42 -0400

    change hyprland input sensitivity

--- configuration/soda/config/hyprland.nix
+++ configuration/soda/config/hyprland.nix
@@ -260,7 +260,7 @@ in
             numlock_by_default = false;
             repeat_rate = 25;
             repeat_delay = 500;
-            sensitivity = 1.0;
+            sensitivity = 1.2;
             accel_profile = "flat";
             force_no_accel = false;
             left_handed = false;
