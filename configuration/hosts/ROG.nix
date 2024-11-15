e1749da7c16947d9665b48b51d45b490a56d6306
meirisoda <meirisoda@proton.me>
2024-11-15 15:51:52 -0500

    change auto-cpufreq battery configurations

--- configuration/hosts/ROG.nix
+++ configuration/hosts/ROG.nix
@@ -67,14 +67,14 @@
           scaling_max_freq = 3300000; 
           turbo = "never";
         };
-        # battery = {
-        #   governor = "powersave";
-        #   scaling_min_freq = 400000;
-        #   scaling_max_freq = 2000000;
-        #   turbo = "never";
-        #   enable_thresholds = true;
-        #   stop_threshold = 95;
-        # };
+        battery = {
+          governor = "powersave";
+          scaling_min_freq = 400000;
+          scaling_max_freq = 2000000;
+          turbo = "never";
+          enable_thresholds = true;
+          stop_threshold = 95;
+        };
       };
     };
   };
