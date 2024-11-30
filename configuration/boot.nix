407a52002d01c6c5a2e641e229fd347f668eed63
meirisoda <meirisoda@proton.me>
2024-11-29 21:10:22 -0500

    from kernel 6.12.0

--- configuration/boot.nix
+++ configuration/boot.nix
@@ -11,24 +11,24 @@
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
-    # kernelPackages = pkgs.linuxPackages_latest; # manually update kernel // run uname -a to see current version
+    kernelPackages = pkgs.linuxPackages_latest; # manually update kernel // run uname -a to see current version
     # kernelPackages = kernelpkgs.linuxPackages_6_10; #manually switching to 6.10 to fix ppd issue with 6.11 (reported to kernel maintainers)
     
-    kernelPackages = 
-      let
-        linux_hardened_pkg = { fetchurl, buildLinux, ... } @ args:
+    # kernelPackages = 
+    #   let
+    #     linux_hardened_pkg = { fetchurl, buildLinux, ... } @ args:
 
-          buildLinux (args // rec {
-            version = "6.9-test";
-            modDirVersion = "6.12.0";
+    #       buildLinux (args // rec {
+    #         version = "6.9-test";
+    #         modDirVersion = "6.12.0";
 
-            src = "${attrs.linux-kernel}";
-            ignoreConfigErrors = true;
+    #         src = "${attrs.linux-kernel}";
+    #         ignoreConfigErrors = true;
 
-            extraMeta.branch = "6.9";
-          } // (args.argsOverride or {}));
-        linux_hardened = pkgs.callPackage linux_hardened_pkg{};
-      in
-        pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor linux_hardened);
+    #         extraMeta.branch = "6.9";
+    #       } // (args.argsOverride or {}));
+    #     linux_hardened = pkgs.callPackage linux_hardened_pkg{};
+    #   in
+    #     pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor linux_hardened);
   };
 }
