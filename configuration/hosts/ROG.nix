b59a4acdbb79bb4b6ef768345dfdebed606c74ec
meirisoda <meirisoda@proton.me>
2024-12-03 17:58:22 -0500

    re-enable powertop

--- configuration/hosts/ROG.nix
+++ configuration/hosts/ROG.nix
@@ -54,9 +54,9 @@
     };
   };
 
-  # powerManagement = {
-  #   powertop.enable = true;
-  # };
+  powerManagement = {
+    powertop.enable = true;
+  };
 
   # programs = {
   #   auto-cpufreq = {
