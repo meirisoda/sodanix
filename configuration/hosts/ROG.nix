87e3de6b4f398ea56009f8f48c05e638ccf94cab
meirisoda <meirisoda@proton.me>
2024-12-06 15:36:31 -0500

    tlp config?

--- configuration/hosts/ROG.nix
+++ configuration/hosts/ROG.nix
@@ -18,7 +18,7 @@
       enableUserService = true; 
       package = stablepkgs.asusctl;
     };
-    power-profiles-daemon.enable = true;
+    # power-profiles-daemon.enable = true;
     thermald.enable = true;
     tlp = {
       enable = true;
@@ -26,11 +26,11 @@
         START_CHARGE_THRESH_BAT0 = 40;
         STOP_CHARGE_THRESH_BAT0 = 95; 
 
-        # CPU_SCALING_GOVERNOR_ON_AC = "balanced";
-        # CPU_ENERGY_PERF_POLICY_ON_AC = "balanced";
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "balanced";
 
-        # CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-        # CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+        CPU_SCALING_GOVERNOR_ON_BAT = "balanced";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "powersave";
         
         CPU_MIN_PERF_ON_AC = 0;
         CPU_MAX_PERF_ON_AC = 100;
